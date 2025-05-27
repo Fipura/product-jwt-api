8a6fa487cb37d8a0bc56bdc9ebc294a5cd3356d3
Pedro Ferreira <Fipura@gmail.com>
2025-05-27 13:20:33 +0100

    Add JWT service and secret config

---
+++ src/main/java/com/pedro/productjwtapi/service/JwtService.java
@@ -0,0 +1,62 @@
+package com.pedro.productjwtapi.service;
+
+import com.pedro.productjwtapi.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${spring.security.jwt.secret}")
+    private String secret;
+
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .claim("role", user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, User user){
+        final String username = extractUsername(token);
+        return (username.equals(user.getUsername()) && !isTokenExprired(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Key getSignKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+}
