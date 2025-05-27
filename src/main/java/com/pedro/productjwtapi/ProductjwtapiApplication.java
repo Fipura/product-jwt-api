e166b11336f09e3fc0546a4a9fd3d6c229f85b28
Pedro Ferreira <Fipura@gmail.com>
2025-05-27 12:40:16 +0100

    Initial commit - Spring Boot JWT project setup

---
+++ src/main/java/com/pedro/productjwtapi/ProductjwtapiApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.productjwtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductjwtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductjwtapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/productjwtapi/ProductjwtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.productjwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductjwtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
